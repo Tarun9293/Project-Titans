da4953f347e820e599f9658f6b79b28dc91ed909
Vijay <vijayarajanb@hotmail.com>
2021-06-30 22:38:15 +0530

    style changes - partially

--- src/components/Play/Play.js
+++ src/components/Play/Play.js
@@ -7,6 +7,7 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import {useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import './Play.css'
 
 
 
@@ -14,23 +15,30 @@ function Play() {
   const classes = useStyles();
   const {push}=useHistory()
     return (
-      <div className={classes.root}>
-                <Grid container spacing={3} xs={12}>
-                    <Grid item xs={12}>
-                        {/* <h1>Choose the section to kickstart!!!</h1> */}
-                        <Paper className={classes.paper} style={{backgroundColor: 'transparent',boxShadow: 'none'}}>   
-                        <Button variant="contained" color="primary"
-                            className={classes.button}
-                            onClick={() => push(`/${'General'}`)} style={{ color: 'white', opacity: '1' }}>General</Button>
-
-                        <Button variant="contained" color="primary"
-                            className={classes.button}
-          
-          onClick={() => push(`/${'worldcup'}`)} style={{ color: 'white', opacity: '1' }}>WorldCup</Button>
-          </Paper>
-  </Grid>
-  </Grid>
-
+      <div>
+        <Grid container spacing={3} xs={12}>
+          <Grid item xs={12} alignItems="baseline" className="box">
+            {/* <h1>Choose the section to kickstart!!!</h1> */}
+          <div >
+            <div className="polaroid">
+              <div className="container">
+                <img className="image" src="/src/general.jpg" alt="general"/>
+                <p className="category" onClick={() => push(`/${'General'}`)}>General</p>
+              </div>
+            </div>
+            <div className="polaroid">
+              <div className="container">
+                <p className="category" onClick={() => push(`/${'General'}`)}>T20</p>
+              </div>
+            </div>
+            <div className="polaroid">
+              <div className="container">
+                <p className="category" onClick={() => push(`/${'General'}`)}>Worldcup</p>
+              </div>
+            </div>
+          </div> 
+          </Grid>
+        </Grid>
       </div>
       );
 }
